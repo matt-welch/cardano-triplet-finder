ecfa5130afe98b397825fc33ace54a9e1ee80bad
Matt Welch <mattw0308@gmail.com>
2013-01-26 16:00:25 -0700

    Added timing functionality and omp parallel for with shared and firstprivate variables

--- cardano_triplets.c
+++ cardano_triplets.c
@@ -13,59 +13,57 @@
 # include <stdlib.h>
 # include <math.h>   
 # include <omp.h>
+# include <time.h>
+#include <sys/time.h>
 int main(int argc, char* argv[]) {
+    /* numc variables */
     int a,b,n, count=0 ;
     double numer, denom, c;   
-    double term1,exp1,test1,test2,eps ;   
-    eps = 1e-11;
+    double eps;
+    
+    /* timing variables */
+    struct timeval start, end;
+    long seconds, useconds;
+    double preciseTime;
+
+    /* Get start time */
+    gettimeofday(&start, NULL);
+
+#if 0
+    double term1,exp1,test1,test2;   
     exp1=1.0/3.0;
+#endif
+    eps = 1e-11;
     n=atoi(argv[1]);
     for(a = 1 ; a < n ; a++)   {   
 #ifdef PARALLEL
-#pragma omp parallel for shared(count) 
+#pragma omp parallel for reduction(+:count) private(b, numer, denom, c) firstprivate(n, a, eps)
 #endif
         for(b = 1 ; b < n ; b++)   {   
             /* Solve for c, determine if it is an integer */
             numer = ((a+1) * (a+1)) * (8*a - 1); 
             denom = 27 * b * b;
             c = (double)numer / (double)denom;
-          //  printf ("%i %i %f\n",a,b,c);
-            
+#ifdef DEBUG
+            printf ("%i %i %f\n",a,b,c);
+#endif            
            
             if(c > 0 && ((c - (int)c)) < eps){
                 /* if c is an integer, we've found a triplet */
-                if( (a + b + c ) < n ) {
+                if( (a + b + c ) <= n ) {
                     /* verify a + b + c < n  (limit)*/ 
                     count++;
                     printf ("%i %i %i\n",a,b,(int)c);
                 }
             }
-#if 0
-            for(c = 1 ; c < n ; c++)   {   
-                if (a+b+c<n) {
-                    term1=(double)b*sqrt((double)c);
-                    test1=(double)a-term1;
-                    test2=(double)a+term1;
-                if (test1>0) {
-                	if (fabs(pow(test2,exp1)+pow(test1,exp1)-1.0)<eps) {
-                	    count++;
-                	    printf ("%i %i %i\n",a,b,c);
-                	}
-                } else 
-                    {
-                    /* branch testing if:::::: */
-                    if (fabs(pow(test2,exp1)-pow(-test1,exp1)-1.0)<eps)
-                        {
-                            count++;
-                	        printf ("%i %i %i\n",a,b,c);
-                	    }
-                    }
-                }
-            }
-
-#endif
-        }
+       }
+/* end parallel for */
     }   
-    printf("Total = %d \n", count) ;   
+    printf("Total = %d \n", count) ; 
+    gettimeofday(&end, NULL);
+    seconds  = end.tv_sec  - start.tv_sec;
+    useconds = end.tv_usec - start.tv_usec;
+    preciseTime = seconds + useconds/1000000.0;
+    printf("Total Time = %3.4f\n", preciseTime );  
     return 0;
 }   
