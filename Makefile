e87acbdbdbde4105867a8841bed87b8643daf730
Matt Welch <matt-welch@users.noreply.github.com>
2013-01-26 13:49:33 -0700

    Removed inner loop in favor of solving for C then making sure C is (1) not negative (2) an integer and verifying: a + b + c < n Added an ifdef and pragma parallel for with shared(count). parallel version currently runs slower than serial 	- probably as a result of the shared count variable Need a better way to split up the domain of a & b.

--- Makefile
+++ Makefile
@@ -1,22 +1,26 @@
 cardano: 
 	gcc cardano_triplets.c -o cardano $(CONFIG)
 
+parallel:
+	gcc cardano_triplets.c -o cardano $(PARCONFIG)
+
 clean:
 	rm -f cardano *.o core core.*
 
 tidy: clean
 	rm -f *.*~ *~
 
-DEBUG_FLAGS = -g3 -ggdb -O0 -Wall -pedantic -DDEBUG -lpthread -lm
-CONFIG		= -Wall -pedantic -lpthread -O3 -lm
-EASY_FLAGS	= -lpthread -lm
+DEBUG_FLAGS = -g3 -ggdb -O0 -Wall -pedantic -DDEBUG -fopenmp -lm
+CONFIG		= -Wall -pedantic -fopenmp -O3 -lm
+PARCONFIG	= $(CONFIG) -DPARALLEL
+EASY_FLAGS	= -fopenmp -lm
 
 debug: CONFIG=$(DEBUG_FLAGS)
-debug: cadrano
+debug: cardano
 
 easy: CONFIG=$(EASY_FLAGS)
 easy: cardano
 
 test: debug
-	./cardano
+	time ./cardano 100
 	
--- cardano_triplets.c
+++ cardano_triplets.c
@@ -10,34 +10,60 @@
  *******************************************************************************/
 
 # include <stdio.h>  
+# include <stdlib.h>
 # include <math.h>   
+# include <omp.h>
 int main(int argc, char* argv[]) {
-    int i,j,k,n, count=0 ;   
+    int a,b,n, count=0 ;
+    double numer, denom, c;   
     double term1,exp1,test1,test2,eps ;   
     eps = 1e-11;
     exp1=1.0/3.0;
     n=atoi(argv[1]);
-    for(i = 1 ; i < n ; i++)   {   
-        for(j = 1 ; j < n ; j++)   {   
-            for(k = 1 ; k < n ; k++)   {   
-                if (i+j+k<n) {
-                    term1=(double)j*sqrt((double)k);
-                    test1=(double)i-term1;
-                    test2=(double)i+term1;
+    for(a = 1 ; a < n ; a++)   {   
+#ifdef PARALLEL
+#pragma omp parallel for shared(count) 
+#endif
+        for(b = 1 ; b < n ; b++)   {   
+            /* Solve for c, determine if it is an integer */
+            numer = ((a+1) * (a+1)) * (8*a - 1); 
+            denom = 27 * b * b;
+            c = (double)numer / (double)denom;
+          //  printf ("%i %i %f\n",a,b,c);
+            
+           
+            if(c > 0 && ((c - (int)c)) < eps){
+                /* if c is an integer, we've found a triplet */
+                if( (a + b + c ) < n ) {
+                    /* verify a + b + c < n  (limit)*/ 
+                    count++;
+                    printf ("%i %i %i\n",a,b,(int)c);
+                }
+            }
+#if 0
+            for(c = 1 ; c < n ; c++)   {   
+                if (a+b+c<n) {
+                    term1=(double)b*sqrt((double)c);
+                    test1=(double)a-term1;
+                    test2=(double)a+term1;
                 if (test1>0) {
                 	if (fabs(pow(test2,exp1)+pow(test1,exp1)-1.0)<eps) {
                 	    count++;
-                	    printf ("%i %i %i\n",i,j,k);
+                	    printf ("%i %i %i\n",a,b,c);
                 	}
-                }
-                else {
-                	    if (fabs(pow(test2,exp1)-pow(-test1,exp1)-1.0)<eps) {
-                	        count++;
-                	        printf ("%i %i %i\n",i,j,k);
+                } else 
+                    {
+                    /* branch testing if:::::: */
+                    if (fabs(pow(test2,exp1)-pow(-test1,exp1)-1.0)<eps)
+                        {
+                            count++;
+                	        printf ("%i %i %i\n",a,b,c);
                 	    }
                     }
                 }
             }
+
+#endif
         }
     }   
     printf("Total = %d \n", count) ;   
